7059e7fcf4cec4a8d9f642bd7aeccb434f4c1cdf
Ivoire Morrell <imorrell@ltu.edu>
2019-12-10 11:20:48 -0500

    View updates

--- BetBank/wwwroot/js/site.js
+++ BetBank/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
