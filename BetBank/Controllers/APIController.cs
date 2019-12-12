670cadcded29d44a13a132a09c03248804f922a4
chrishasbeard <chrisholmes16@gmail.com>
2019-12-12 13:52:47 -0500

    UserData Creation and started the refresh system

--- BetBank/Controllers/APIController.cs
+++ BetBank/Controllers/APIController.cs
@@ -24,38 +24,7 @@ namespace BetBank.Controllers
         }
 
         //initial event data gathering to put into database
-        public async Task<IActionResult> GetSportEvents(int id)
-        {
-            //nfl id = 2, nba = 4
-            if (id == 0)
-            {
-                id = 4;
-            }
-            var response = await _client.GetAsync($"/sports/{id}/events?include=scores");
-            var content = await response.Content.ReadAsAsync<EventsRootobject>(); PopulateSportEvents(content); return View("Index");
-        }
-        public void PopulateSportEvents(EventsRootobject eventsObject)
-        {
-            string HomeTeam;
-            string AwayTeam;
-            foreach (Event item in eventsObject.events)
-            {
-
-                if (item.teams[0].is_home)
-                {
-                    HomeTeam = item.teams[0].name;
-                    AwayTeam = item.teams[1].name;
-                }
-                else
-                {
-                    HomeTeam = item.teams[1].name;
-                    AwayTeam = item.teams[0].name;
-                }
-                EventsTable tempEventRecord = new EventsTable(item.event_id, item.sport_id, item.event_date, item.score.event_status, HomeTeam, item.score.score_home, AwayTeam, item.score.score_away, TimeSpan.Parse(item.score.game_clock.ToString()), item.lines._1.moneyline.moneyline_home, item.lines._1.moneyline.moneyline_away, item.lines._1.spread.point_spread_home, item.lines._1.spread.point_spread_away, item.lines._1.total.total_over, item.lines._1.total.total_under, item.lines._1.line_id, item.lines._1.spread.point_spread_away_money, item.lines._1.spread.point_spread_home_money,item.lines._1.total.total_over_money,item.lines._1.total.total_under_money);
-                _context.EventsTable.Add(tempEventRecord);
-                _context.SaveChanges();
-            }
-        }
+        
 
         public IActionResult Index()
         {
